f08be52d0c439f7b22f21246fb6892cf3e96543c
GPT4RTL <GPT4RTL>
2023-09-09 19:22:21 -0400

    TEST 23 & 24 COMBINED

--- ft_fifo/sva/fifo_prop.sv
+++ ft_fifo/sva/fifo_prop.sv
@@ -141,5 +141,57 @@ assert property (!(|fifo.buffer_val_reg) |-> !fifo.out_val);
 
 
 
+// Check: When there's an input handshake, the next value of buffer_head_reg increments.
+as__buffer_head_increment: assert property (
+    fifo.in_hsk |=> fifo.buffer_head_reg == $past(fifo.buffer_head_reg + 1'b1)
+);
+
+// Check: When there's an output handshake, the next value of buffer_tail_reg increments.
+as__buffer_tail_increment: assert property (
+    fifo.out_hsk |=> fifo.buffer_tail_reg == $past(fifo.buffer_tail_reg + 1'b1)
+);
+
+// Check: When the FIFO is empty (all buffer values are zero), out_val signal should be 0.
+as__out_val_when_empty: assert property (
+    !(|fifo.buffer_val_reg) |-> !fifo.out_val
+);
+
+// Check: When the FIFO is full (all buffer values are one), in_rdy signal should be 0.
+as__in_rdy_when_full: assert property (
+    &fifo.buffer_val_reg |-> !fifo.in_rdy
+);
+
+// Check: If an add_buffer flag is set for a specific index, the corresponding buffer_val_reg should be set in the next cycle.
+generate
+    for (genvar i = 0; i < INFLIGHT; i = i + 1) begin: check_add_buffer_set
+        as__buffer_val_set_when_add_buffer: assert property (
+            fifo.add_buffer[i] |=> fifo.buffer_val_reg[i]
+        );
+    end
+endgenerate
+
+// Check: If an clr_buffer flag is set for a specific index, the corresponding buffer_val_reg should be clear in the next cycle.
+generate
+    for (genvar i = 0; i < INFLIGHT; i = i + 1) begin: check_clr_buffer_clear
+        as__buffer_val_clear_when_clr_buffer: assert property (
+            fifo.clr_buffer[i] |=> !fifo.buffer_val_reg[i]
+        );
+    end
+endgenerate
+
+// Check: When add_buffer flag is set for an index, the next value of buffer_data_reg at that index should be in_data.
+generate
+    for (genvar i = 0; i < INFLIGHT; i = i + 1) begin: check_buffer_data_when_add_buffer
+        as__buffer_data_set_when_add_buffer: assert property (
+            fifo.add_buffer[i] |=> fifo.buffer_data_reg[i] == $past(fifo.in_data)
+        );
+    end
+endgenerate
+
+// Check: out_data should always be the value of buffer_data_reg at the index of buffer_tail_reg.
+as__out_data_matches_tail_data: assert property (
+    fifo.out_data == fifo.buffer_data_reg[fifo.buffer_tail_reg]
+);
+
 
 endmodule
