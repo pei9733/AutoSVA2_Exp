0532c65956bc3240b7cef5fc1fcc9955bc7063ee
GPT4RTL <GPT4RTL>
2023-09-10 13:14:16 -0400

    RTL-GEN 2

--- ft_fifo_gen/QUERY_RTL.md
+++ ft_fifo_gen/QUERY_RTL.md
@@ -1,83 +1,61 @@
-Implementing a simple FIFO that can handle simultaneous push and pop requires maintaining read and write pointers, as well as storage (typically an array or memory). Here's a synthesizable Verilog implementation of the module you've defined, using the provided parameters:
-
 ```verilog
-module fifo_gen
-  #(
-    parameter INFLIGHT_IDX = 2, // Log2 of the number of slots
-    parameter SIZE = 4 // Width of the data slot
-  )
-(
-    // Clock + Reset
-    input  wire                          clk,
-    input  wire                          rst_n,
-
-    input  wire                          in_val,
-    output wire                          in_rdy,
-    input  wire [SIZE-1:0]               in_data,
-
-    output wire                          out_val,
-    input  wire                          out_rdy,
-    output wire [SIZE-1:0]               out_data
-);
-
-localparam INFLIGHT = 2**INFLIGHT_IDX;
-localparam ADDR_SIZE = INFLIGHT_IDX;
-
-// FIFO storage
+// FIFO storage registers declaration
 reg [SIZE-1:0] fifo_storage_reg[INFLIGHT-1:0];
 
-// Read and write pointers
-reg [ADDR_SIZE-1:0] wr_ptr_reg = 0, rd_ptr_reg = 0;
-reg full_reg = 0, empty_reg = 1;
+// Write and read pointers registers
+reg [INFLIGHT_IDX-1:0] wr_ptr_reg = 0, rd_ptr_reg = 0;
 
-// Temporary register to hold data
-reg [SIZE-1:0] out_data_reg;
+// Control registers
+reg empty_reg = 1, full_reg = 0;
 
+// Pointer calculations
+wire [INFLIGHT_IDX:0] next_wr_ptr = wr_ptr_reg + 1'b1;
+wire [INFLIGHT_IDX:0] next_rd_ptr = rd_ptr_reg + 1'b1;
+
+// Update the FIFO status flags
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-        wr_ptr_reg <= 0;
-        rd_ptr_reg <= 0;
-        full_reg <= 0;
-        empty_reg <= 1;
+        empty_reg <= 1'b1;
+        full_reg <= 1'b0;
     end else begin
-        // Handle input
-        if (in_val && in_rdy && !full_reg) begin
-            fifo_storage_reg[wr_ptr_reg] <= in_data;
-            wr_ptr_reg <= wr_ptr_reg + 1;
-        end
-
-        // Handle output
-        if (out_val && out_rdy && !empty_reg) begin
-            out_data_reg <= fifo_storage_reg[rd_ptr_reg];
-            rd_ptr_reg <= rd_ptr_reg + 1;
-        end
-
-        // Handle full and empty flags
-        if ((wr_ptr_reg == rd_ptr_reg - 1) || (wr_ptr_reg == INFLIGHT-1 && rd_ptr_reg == 0)) begin
-            full_reg <= 1;
-        end else if (wr_ptr_reg == rd_ptr_reg) begin
-            empty_reg <= 1;
-        end else begin
-            full_reg <= 0;
-            empty_reg <= 0;
+        // Handle the case where both push and pop occur simultaneously
+        if (in_val && in_rdy && out_val && out_rdy) begin
+            // No change in full and empty flags
+        end else if (in_val && in_rdy) begin // Data is pushed
+            if (next_wr_ptr == rd_ptr_reg) full_reg <= 1'b1;
+            empty_reg <= 1'b0;
+        end else if (out_val && out_rdy) begin // Data is popped
+            if (wr_ptr_reg == next_rd_ptr) empty_reg <= 1'b1;
+            full_reg <= 1'b0;
         end
     end
 end
 
+// Write logic
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) wr_ptr_reg <= 0;
+    else if (in_val && in_rdy && !full_reg) begin
+        fifo_storage_reg[wr_ptr_reg] <= in_data;
+        wr_ptr_reg <= next_wr_ptr[INFLIGHT_IDX-1:0]; // Wrapping around logic is inherently handled by the overflow of the pointer
+    end
+end
+
+// Read logic
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) rd_ptr_reg <= 0;
+    else if (out_val && out_rdy && !empty_reg) begin
+        rd_ptr_reg <= next_rd_ptr[INFLIGHT_IDX-1:0]; // Wrapping around logic is inherently handled by the overflow of the pointer
+    end
+end
+
+// Output data assignment
+assign out_data = fifo_storage_reg[rd_ptr_reg];
+
+// Ready and valid signals
 assign in_rdy = !full_reg;
 assign out_val = !empty_reg;
-assign out_data = out_data_reg;
 
 endmodule
 ```
 
-In this code:
-
-- We use an array `fifo_storage_reg` as our storage for the FIFO.
-- The `wr_ptr_reg` and `rd_ptr_reg` registers act as our write and read pointers respectively.
-- The `full_reg` and `empty_reg` flags indicate when the FIFO is full or empty.
-- We handle simultaneous push/pop by having separate conditions to check the input and output readiness within the always block.
-- We use the `in_rdy` flag to signal the upstream module when it can send data and the `out_val` flag to signal the downstream module when data is available.
-- The `out_data_reg` is used to immediately provide the output data when `out_val` is set, meeting the requirement of the output data being available in the same cycle.
-
-Note: This design assumes that when `out_rdy` is asserted while `out_val` is high, the data will be popped. If there's a specific behavior in your system where `out_rdy` can be high without actually reading the data, then this design might not be suitable.
+Note: This implementation uses synchronous resets (i.e., the state variables are set to their initial values on the rising edge of the clock when the reset is not asserted).
--- quickstart/fifo_gen.v
+++ quickstart/fifo_gen.v
@@ -7,7 +7,48 @@ This is the Specification of a FIFO queue.
 - pop_data MUST BE usable the same cycle that pop_val is active.
 - The module MUST handle push and pop at the same time.
 
+// If input is valid and the FIFO is not full, then the FIFO should be ready to accept the data.
+as__input_ready_when_not_full:
+    assert property (fifo_gen.in_val && !fifo_gen.full_reg |-> fifo_gen.in_rdy);
+
+// If output is requested and the FIFO is not empty, then output should be valid.
+as__output_valid_when_not_empty:
+    assert property (!fifo_gen.empty_reg |-> fifo_gen.out_val);
+
+// If input is valid and FIFO is ready (not full), then the write pointer should increment in the next cycle.
+as__write_pointer_increment:
+    assert property (fifo_gen.in_val && fifo_gen.in_rdy |=> $past(fifo_gen.wr_ptr_reg) + 1'b1 == fifo_gen.wr_ptr_reg);
+
+// If output is valid and FIFO is ready (not empty), then the read pointer should increment in the next cycle.
+as__read_pointer_increment:
+    assert property (fifo_gen.out_rdy && fifo_gen.out_val |=> $past(fifo_gen.rd_ptr_reg) + 1'b1 == fifo_gen.rd_ptr_reg);
+
+// If write pointer wraps around, it should not increment but reset to zero.
+as__write_pointer_wrap_around:
+    assert property ((fifo_gen.wr_ptr_reg) == INFLIGHT-1 && fifo_gen.in_val && fifo_gen.in_rdy |=> fifo_gen.wr_ptr_reg == 0);
+
+// If read pointer wraps around, it should not increment but reset to zero.
+as__read_pointer_wrap_around:
+    assert property ((fifo_gen.rd_ptr_reg) == INFLIGHT-1 && fifo_gen.out_rdy && fifo_gen.out_val |=> fifo_gen.rd_ptr_reg == 0);
+
+as__pointers_equal_when_empty:
+    assert property (fifo_gen.in_val && fifo_gen.in_rdy && (((fifo_gen.wr_ptr_reg + 1'b1) % INFLIGHT) == fifo_gen.rd_ptr_reg) && !(fifo_gen.out_val && fifo_gen.out_rdy) |=> fifo_gen.empty_reg);
+
+as__fifo_full_condition:
+    assert property (fifo_gen.out_val && fifo_gen.out_rdy && (((fifo_gen.rd_ptr_reg + 1) % INFLIGHT) == fifo_gen.wr_ptr_reg) && !(fifo_gen.in_val && fifo_gen.in_rdy) |=> fifo_gen.full_reg);
+
+// If FIFO is not empty and output is ready, the out_data should get the value at the current read pointer.
+as__out_data_on_read:
+    assert property (!fifo_gen.empty_reg && fifo_gen.out_rdy |-> fifo_gen.out_data == fifo_gen.fifo_storage_reg[fifo_gen.rd_ptr_reg]);
+
+// When input is valid, data should be written to the FIFO at the current write pointer.
+as__in_data_on_write:
+    assert property (fifo_gen.in_val && fifo_gen.in_rdy |=> fifo_gen.fifo_storage_reg[$past(fifo_gen.wr_ptr_reg)] == $past(fifo_gen.in_data));
+
+
 Implement the following module only using synthesizable Verilog.
+DO NOT write the module interface, only the implementation.
+DO NOT answer anything except for the Verilog implementation, ONLY write comments within the Verilog code.
 Append "_reg" to the name of ALL registers.
 */
 
@@ -31,52 +72,61 @@ module fifo_gen
 );
 
 localparam INFLIGHT = 2**INFLIGHT_IDX;
-localparam ADDR_SIZE = INFLIGHT_IDX;
-
-// FIFO storage
+// FIFO storage registers declaration
 reg [SIZE-1:0] fifo_storage_reg[INFLIGHT-1:0];
 
-// Read and write pointers
-reg [ADDR_SIZE-1:0] wr_ptr_reg = 0, rd_ptr_reg = 0;
-reg full_reg = 0, empty_reg = 1;
+// Write and read pointers registers
+reg [INFLIGHT_IDX-1:0] wr_ptr_reg = 0, rd_ptr_reg = 0;
 
-// Temporary register to hold data
-reg [SIZE-1:0] out_data_reg;
+// Control registers
+reg empty_reg = 1, full_reg = 0;
 
+// Pointer calculations
+wire [INFLIGHT_IDX:0] next_wr_ptr = wr_ptr_reg + 1'b1;
+wire [INFLIGHT_IDX:0] next_rd_ptr = rd_ptr_reg + 1'b1;
+
+// Update the FIFO status flags
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-        wr_ptr_reg <= 0;
-        rd_ptr_reg <= 0;
-        full_reg <= 0;
-        empty_reg <= 1;
+        empty_reg <= 1'b1;
+        full_reg <= 1'b0;
     end else begin
-        // Handle input
-        if (in_val && in_rdy && !full_reg) begin
-            fifo_storage_reg[wr_ptr_reg] <= in_data;
-            wr_ptr_reg <= wr_ptr_reg + 1;
-        end
-
-        // Handle output
-        if (out_val && out_rdy && !empty_reg) begin
-            out_data_reg <= fifo_storage_reg[rd_ptr_reg];
-            rd_ptr_reg <= rd_ptr_reg + 1;
-        end
-
-        // Handle full and empty flags
-        if ((wr_ptr_reg == rd_ptr_reg - 1) || (wr_ptr_reg == INFLIGHT-1 && rd_ptr_reg == 0)) begin
-            full_reg <= 1;
-        end else if (wr_ptr_reg == rd_ptr_reg) begin
-            empty_reg <= 1;
-        end else begin
-            full_reg <= 0;
-            empty_reg <= 0;
+        // Handle the case where both push and pop occur simultaneously
+        if (in_val && in_rdy && out_val && out_rdy) begin
+            // No change in full and empty flags
+        end else if (in_val && in_rdy) begin // Data is pushed
+            if (next_wr_ptr == rd_ptr_reg) full_reg <= 1'b1;
+            empty_reg <= 1'b0;
+        end else if (out_val && out_rdy) begin // Data is popped
+            if (wr_ptr_reg == next_rd_ptr) empty_reg <= 1'b1;
+            full_reg <= 1'b0;
         end
     end
 end
 
+// Write logic
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) wr_ptr_reg <= 0;
+    else if (in_val && in_rdy && !full_reg) begin
+        fifo_storage_reg[wr_ptr_reg] <= in_data;
+        wr_ptr_reg <= next_wr_ptr[INFLIGHT_IDX-1:0]; // Wrapping around logic is inherently handled by the overflow of the pointer
+    end
+end
+
+// Read logic
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) rd_ptr_reg <= 0;
+    else if (out_val && out_rdy && !empty_reg) begin
+        rd_ptr_reg <= next_rd_ptr[INFLIGHT_IDX-1:0]; // Wrapping around logic is inherently handled by the overflow of the pointer
+    end
+end
+
+// Output data assignment
+assign out_data = fifo_storage_reg[rd_ptr_reg];
+
+// Ready and valid signals
 assign in_rdy = !full_reg;
 assign out_val = !empty_reg;
-assign out_data = out_data_reg;
 
 endmodule
 
