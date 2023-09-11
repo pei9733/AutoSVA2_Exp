ae4202fe6037f1313eb5d36a37a2584bb5d72d9f
GPT4RTL <GPT4RTL>
2023-09-10 21:50:59 -0400

    Script to Generate Prompt File

--- SVA_FOOTER.v
+++ SVA_FOOTER.v
@@ -1,7 +1,7 @@
 /*
 RULES:
-SVA assertions are written within a property file, but DO NOT rewrite the module interface and DO NOT add includes in the property file (as we already have them in the property file).
-DO NOT declare properties; DECLARE assertions named as__<NAME>: assert property (<EXPRESSION>).
+SVA assertions are written within a property file, but DO NOT rewrite the MODULE interface and DO NOT add includes in the property file (as we already have them in the property file).
+DO NOT declare properties; DECLARE assertions named asgpt__<NAME>: assert property (<EXPRESSION>).
 DO NOT use [] at the end of assertion NAME. Do not add @(posedge clk) to EXPRESSION.
 Assertions must be as high-level as possible, to avoid repeating implementation details.
 Above each assertion, write a comment explaining behavior being checked.
@@ -11,7 +11,6 @@ $countones(bitarray) returns the number of ONES in bitarray.
 !(&bitarray) means it's NOT TRUE that ALL the bits are ONES, i.e., SOME of the bits are ZEROS.
 |bitarray means that SOME bits is ONES.
 !(|bitarray) means that NONE of the bits are ONES, i.e., ALL the bits are ZEROS.
-
 
 Same-cycle assertions (|->): the precondition and postcondition are evaluated in the same cycle.
 Next-cycle assertions (|=>): the precondition is evaluated in the current cycle and the postcondition in the next cycle.
@@ -29,12 +28,14 @@ On the postcondition of next-cycle assertions (|=>), DO NOT USE $past() to refer
 On the postcondition of next-cycle assertions (|=>), USE $past() to refer to the value of the _reg on the cycle of the precondition, before the register update.
 Assertions without precondition DO NOT use |->
 
-Internal signals are those signals NOT present in the module interface. Internal signals are declared within the module.
-Referencing internal signals in the property file ALWAYS requires prepending the name of the module before the signal name, e.g., name.<internal_signal>.
-NEVER reference internal signals without the module name prefix, e.g., name.<internal_signal>.
 DO NOT use foreach loops in assertions; Instead, use generate loops.
+Interface signals are those declared as input or output, in the module interface.
+Internal signals are declared within the MODULE implementation body.
+Referencing internal signals in the property file ALWAYS requires prepending MODULE before the signal name as MODULE.<internal_signal>
+NEVER reference internal signals without MODULE as prefix, e.g., MODULE.<internal_signal>
+
 
 TASK:
-Write SVA assertions to check correctness of ALL the functionality of the module but the reset behavior.
+Write SVA assertions to check correctness of ALL the functionality of MODULE but the reset behavior.
 DO NOT answer anything except for the property file, ONLY write comments within the property file.
 */
